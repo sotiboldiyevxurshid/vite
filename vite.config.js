ccf538d88febdb65a100be8c516d602e2071622d
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-08-17 15:28:36 +0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: '/docs/index.html'
+    origin: 'http://127.0.0.1:8080'
   }
+  
 })
