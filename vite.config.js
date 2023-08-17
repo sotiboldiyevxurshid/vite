ec9ac50eacf38384b34bf124cb396d563eab5463
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-08-17 16:11:24 +0500

    d

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8099
+    port: 3030,
   },
   preview: {
-    port: 8099
-  }
+    port: 8080,
+  },
 })
 
 
